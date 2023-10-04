6e89fac2d8e408b0f81304cc49c19c019651d77f
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-10-04 15:09:24 +0200

    Add linebreak in title

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -28,7 +28,7 @@ $initialMessage = rand(0, count($messages)-1);
 <x-app-layout class="main-app-screen">
     <x-screen-layout id="start" class="current-screen">
         <div class="nv-action-section-content">
-            <h1>Cool, dass du dabei bist!</h1>
+            <h1>Cool,<br>dass du dabei bist!</h1>
             <p>Damit dieses Jahr nicht wieder nur 27% der Bevölkerung entscheiden, wen der Kanton Zürich nach Bern schickt, müssen wir Menschen an die Urnen mobilisieren.</p>
             <p class="!text-lg"> Wenn wir den Rechtsrutsch im Parlament verhindern wollen, dann müssen wir alle unser Umfeld daran erninnern, jetzt GRÜNE Liste 3 zu wählen. <b>So kannst du mitmachen:</b></p>
             <div class="nv-buttongrid mt-8">
